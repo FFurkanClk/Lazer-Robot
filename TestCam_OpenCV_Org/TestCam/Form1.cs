c8ea4712c89b3f9c3b7a510628134cd6ab001a3a
FurkanCelik <111591874+FFurkanClk@users.noreply.github.com>
2023-12-22 16:42:58 +0300

    Add files via upload

---
+++ TestCam_OpenCV_Org/TestCam/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace TestCam
+{
+    public partial class Form1 : Form
+    {
+        private Capture capture;
+        private Image<Bgr, Byte> IMG;
+        
+        
+        private Image<Gray, Byte> R_frame;
+        private Image<Gray, Byte> G_frame;
+        private Image<Gray, Byte> B_frame;
+        private Image<Gray, Byte> GrayImg;
+
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void processFrame(object sender, EventArgs e)
+        {
+            if (capture == null)//very important to handel excption
+            {
+                try
+                {
+                    capture = new Capture(); 
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+
+            IMG = capture.QueryFrame();
+
+            
+            R_frame = IMG[2].Copy();
+            G_frame = IMG[1].Copy();
+            B_frame = IMG[0].Copy();            
+            GrayImg = IMG.Convert<Gray, Byte>();
+      
+            try
+            {
+                
+                imageBox1.Image = IMG;
+                imageBox2.Image = GrayImg;
+                imageBox3.Image = R_frame;
+                imageBox4.Image = G_frame;
+                imageBox5.Image = B_frame;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Idle += processFrame;
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= processFrame;
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }    
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IMG.Save("Image" + ".jpg");
+        }       
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        
+    }
+}
